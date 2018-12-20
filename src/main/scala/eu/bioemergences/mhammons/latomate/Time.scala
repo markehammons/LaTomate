c734ed2234e196c1335b824a8f0038e0939644c7
mhammons <mark.hammons@inaf.cnrs-gif.fr>
2018-12-20 06:40:48 +0100

    Closes #1 and fixes the default look of the fxml to use 00m 00s.

--- src/main/scala/eu/bioemergences/mhammons/latomate/Time.scala
+++ src/main/scala/eu/bioemergences/mhammons/latomate/Time.scala
@@ -2,6 +2,6 @@ package eu.bioemergences.mhammons.latomate
 
 object Time {
   def prettyPrintMillis(millis: Long) = {
-    s"${millis / 60 / 1000}m ${millis / 1000 % 60}s"
+    f"${millis / 60 / 1000}%02dm ${millis / 1000 % 60}%02ds"
   }
 }
