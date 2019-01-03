4a26cf460e9ff3e5ef75d4c8ac49d3ea42d68083
mhammons <mark.hammons@inaf.cnrs-gif.fr>
2019-01-03 22:03:42 +0100

    Work on #18

--- LaTomate/test/src/io/github/markehammons/latomate/controllers/PomodoroControllerImplSpecification.scala
+++ LaTomate/test/src/io/github/markehammons/latomate/controllers/PomodoroControllerImplSpecification.scala
@@ -1,11 +1,9 @@
 package io.github.markehammons.latomate.controllers
 
 import akka.actor.testkit.typed.scaladsl.TestInbox
-import io.github.markehammons.latomate.models.RootModel.{
-  RootVocabulary,
-  SpawnPomodoroModel
-}
+import io.github.markehammons.latomate.models.RootModel.{RootVocabulary, SpawnPomodoroModel}
 import io.github.markehammons.latomate.models.pomodoro
+import io.github.markehammons.latomate.tags.GUI
 import javafx.application.Platform
 import org.scalatest.{BeforeAndAfterAll, DiagrammedAssertions, WordSpec}
 
@@ -27,25 +25,25 @@ class PomodoroControllerImplSpecification
   Thread.sleep(150)
 
   "An FXMLPomodoroControllerImpl" should {
-    "request a timer model from the root model on start" in {
+    "request a timer model from the root model on start" taggedAs GUI in {
       pomodoroControllerImpl.initialize()
 
       rootMailbox.expectMessage(SpawnPomodoroModel(pomodoroControllerImpl))
     }
 
-    "send a start message on start function activation" in {
+    "send a start message on start function activation" taggedAs GUI in {
       pomodoroControllerImpl.start()
 
       pomodoroMailbox.expectMessage(pomodoro.Start)
     }
 
-    "send a snooze message on snooze function activation" in {
+    "send a snooze message on snooze function activation" taggedAs GUI in {
       pomodoroControllerImpl.snooze()
 
       pomodoroMailbox.expectMessage(pomodoro.Snooze)
     }
 
-    "send a stop message on stop function activation" in {
+    "send a stop message on stop function activation" taggedAs GUI in {
       pomodoroControllerImpl.stop()
 
       pomodoroMailbox.expectMessage(pomodoro.Stop)
---
+++ LaTomate/test/src/io/github/markehammons/latomate/tags/GUI.scala
@@ -0,0 +1,5 @@
+package io.github.markehammons.latomate.tags
+
+import org.scalatest.Tag
+
+object GUI extends Tag("io.github.markehammons.latomate.tags.GUI")
